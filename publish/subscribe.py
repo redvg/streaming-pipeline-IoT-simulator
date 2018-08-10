05674762480e089ada16e99b587653c0f4007740
redvg <redvg17@gmail.com>
2018-08-10 23:02:45 +0200

    fixes subscriber

--- publish/subscribe.py
+++ publish/subscribe.py
@@ -1,16 +1,40 @@
 from google.cloud import pubsub_v1
 
-subscriber = pubsub_v1.SubscriberClient()
-
 def callback(message):
 
   print('Received message: {}'.format(message))
 
   message.ack()
 
-subscription_path = 'projects/udemy-data-engineer-210920/topics/sensors'
+if __name__ == '__main__':
 
-subscriber.subscribe(subscription_path, callback=callback)
+   parser = argparse.ArgumentParser(description='Creates subscrition')
+   parser.add_argument('--project',
+                       help='Example: --project $DEVSHELL_PROJECT_ID',
+                       required=True)
+   parser.add_argument('--topic',
+                       help='topic name',
+                       required=True)
+   parser.add_argument('--name',
+                       help='subscription name',
+                       required=True)
+
+   args = parser.parse_args()
+
+   subscriber = pubsub_v1.SubscriberClient()
+
+   topic_path = subscriber.topic_path(args.project, args.topic)
+
+   subscription_path = subscriber.subscription_path(args.project, args.name)
+
+   subscription = subscriber.create_subscription(subscription_path,
+                                                 topic_path)
+
+   print('Subscription created: {}'.format(subscription))
+
+   subscriber.subscribe(subscription_path, callback=callback)
+
+   print('Listening for messages on {}'.format(subscription_path))
 
 # just go to https://console.cloud.google.com/cloudpubsub/subscriptions/cpsubs
 # and publish some messages. You will see the payload inmediately on cloudshell
