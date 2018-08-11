adbb7dfaf7a04f287d5273d29362fd759227f8f0
redvg <redvg17@gmail.com>
2018-08-11 15:36:29 +0200

    improves bashes

--- process/run.sh
+++ process/run.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-python pipeline.py --project=$DEVSHELL_PROJECT_ID --bq=iot.sensors --pubsub=sensors
+echo 'launching streaming pipeline'
+echo 'usage: ./run.sh Project BigQueryDataset.Table PubSubTopic'
+
+if ["$#" -lt 3]: then
+  echo 'wrong usage'
+  exit
+fi
+
+PROJECT=$1
+BQ=$2
+PUBSUB=$3
+
+python pipeline.py --project=$PROJECT --bq=$BQ --pubsub=$PUBSUB
+
+echo '+'
--- publish/run.sh
+++ publish/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo 'launching pubsub publisheer'
+
 python sensors.py --speedFactor=60 --project=$DEVSHELL_PROJECT_ID
+
+echo '+'
