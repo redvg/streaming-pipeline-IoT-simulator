2ef5fe3c541d7c65e48691cb4824d795f144761b
redvg <redvg17@gmail.com>
2018-08-11 13:47:17 +0200

    fixes job name

--- process/pipeline.py
+++ process/pipeline.py
@@ -31,7 +31,7 @@ def run():
 
     argv = [
       '--project={0}'.format(args.project),
-      '--job_name=IoTStream',
+      '--job_name=iotstream',
       '--save_main_session',
       '--staging_location=gs://{0}/{1}/staging/'.format(BUCKET_ID, BUCKET_FOLDER),
       '--temp_location=gs://{0}/{1}/staging/'.format(BUCKET_ID, BUCKET_FOLDER),
